fdeab6fb66518da855193a09273090342f2406ea
Air3lax <33313977+Air3lax@users.noreply.github.com>
2023-03-12 18:23:22 +0100

    formatting

--- telegram_send.py
+++ telegram_send.py
@@ -6,4 +6,4 @@ def send_telegram_message(token, chat_id, message):
     chat_id = chat_id
     message = message
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-    print(requests.get(url).json()) # this sends the message
+    print(requests.get(url).json())
