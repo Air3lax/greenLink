3a729c14cdae0de5348961a7a75426c12856fbc1
Air3lax <33313977+Air3lax@users.noreply.github.com>
2023-03-21 19:25:38 +0100

    telegram send logfile

--- telegram_send.py
+++ telegram_send.py
@@ -1,5 +1,6 @@
 import requests
 import config_parser as cp
+import json
 
 
 
@@ -10,3 +11,18 @@ def send_telegram_message(message):
     message = message
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
     requests.get(url).json()
+
+def send_log_file():
+    config_data = cp.read_config()
+    TOKEN = config_data['telegram_credentials']['token']
+    CHAT_ID = config_data['telegram_credentials']['chat_id']
+    document = open('_log.txt', "rb")
+    url = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
+    response = requests.post(url, data={'chat_id': CHAT_ID}, files={'document': document})
+    # part below, just to make human readable response for such noobies as I
+    #content = response.content.decode("utf8")
+    #js = json.loads(content)
+    #print(js)
+
+if __name__ == '__main__':
+    send_log_file()
